d63f116bb80878010a47de1361125ab71ea60622
Lisaliv2610 <lisa.liv.jonsson@gmail.com>
2026-03-13 09:45:30 +0100

    trække flere changes

--- #Bachelor databehandling.py
+++ #Bachelor databehandling.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 
 # Læs data fra CSV-filen
-data = pd.read_csv('data.csv')
+#data = pd.read_csv('data.csv')
 # Vis de første 5 rækker af data
-print(data.head())
+#print(data.head())
